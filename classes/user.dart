b4548f048d26dabbad3b53c95df86401aa3b9202
Hunter Webb <44552711+RedH11@users.noreply.github.com>
2021-06-21 17:23:51 -0500

    event.dart -> Class for the events
    user.dart ->
    Class for info on users

---
+++ classes/user.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/*
+WHEN WE INTEGRATE FIREBASE WE JUST HAVE TO MAKE SURE THE TRAFFIC IS END TO END ENCRYPTED
+ */
+class User {
+  String uid = "";
+  String username = "";
+  String password = "";
+  int coinCount = 0;
+  Map<String, int> investments = {}; // {Event ID, Coin's Invested}
+
+
+}
