f19ff0b6d3dafd72d61fbb006bda8d6ab679efd4
Hunter Webb <44552711+RedH11@users.noreply.github.com>
2021-07-01 15:55:22 -0500

    Added link to guide for dynamically sizing text

--- lib/screens/intro/intro.dart
+++ lib/screens/intro/intro.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cryptoapp/screens/intro/components/body.dart';
 
+///
+/// https://stackoverflow.com/questions/50751226/how-to-dynamically-resize-text-in-flutter
+/// Probably have to dynamically size a lot of the text
+/// 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
